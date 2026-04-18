f3b418bda7170aa7dc2056d355b7f147704749da
Priyanka Singh <personaluse.priyanka@gmail.com>
2026-04-18 10:35:56 +0530

    add

---
+++ Operation.java
@@ -0,0 +1,15 @@
+public class Operation {
+    public static void main(String args[]) {
+        int a = 10;
+        int b = 2;
+        int sum = a + b;
+        int diff = a - b;
+        int mul = a * b;
+        int div = a / b;
+        System.out.println("sum=" + sum);
+        System.out.println("diff=" + diff);
+        System.out.println("mul=" + mul);
+        System.out.println("div=" + div);
+    }
+
+}
